af01f11d540a70edb99235c8407ee0978a339c86
willyserge <willyserge01@gmail.com>
2020-08-31 15:34:29 +0200

    implement app functionality

---
+++ src/Components/Coin/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Coin({name , image , price , symbol , volume}) {
+    return (
+        <div className="coin-container">
+            <div className="coin-row">
+              <div className="coin">
+                <img src={image} alt="crypto"/>
+                <h1>{name}</h1>
+                <p className="coin-symbol">{symbol}</p>
+              </div>
+              <div className="coin-data">
+                <p className="coin-price">{price}</p>
+                <p className="coin-volume">{volume.toLocaleString()}</p>
+              </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coin
